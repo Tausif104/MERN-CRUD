87a24348a421edb50adec691dc7f0a6f6f4d3efd
Tausif104 <tausifahmed49@gmail.com>
2022-01-25 18:04:55 +0600

    final commit

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use('/api/notes', notesRouter)
 
 connectDB()
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${5000}`)
